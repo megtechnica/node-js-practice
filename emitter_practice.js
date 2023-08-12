7dd845b1e8a1f8320e075236af74e0cdf7956479
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-08-12 14:22:48 -0600

    Create emitter_practice.js

---
+++ emitter_practice.js
@@ -0,0 +1,34 @@
+const EventEmitter = require(events);
+cont eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1, num2)=> {
+  console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial', 1, 2);
+
+class Person extends EventEmitter {
+  constructor (){
+    super();
+    this._name = name;
+  }
+
+  get name(){
+    return this._name;
+  }
+}
+
+let pedro = new Person('Pedro');
+let christina = new Person('Christina');
+
+christina.on('name', ()=> {
+  console.log('my name is ' + christina.name);
+});
+
+pedro.on('name', ()=>{
+  console.log('my name is ' + pedro.name);
+});
+
+pedro.emit('name');
+christina.emit('name');
+
