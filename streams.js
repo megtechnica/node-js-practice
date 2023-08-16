db36052dea2dc547cd0fc20825edea507b28262a
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-08-16 10:22:29 -0600

    Update streams.js

--- streams.js
+++ streams.js
@@ -4,5 +4,6 @@ const readStream = fs.createReadStream('./example.txt');
 const writeStream = fs.createWriteStream('example2.txt');
 readStream.on('data', (chunk)=>{
   console.log(chunk);
+  writeStream.wrtie(chunk);
   
 });
