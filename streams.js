eaf298cfea998323b11f2935a8c529e68cf2f5df
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2023-08-15 18:38:38 -0600

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./example.txt');
+const writeStream = fs.createWriteStream('example2.txt');
+readStream.on('data', (chunk)=>{
+  console.log(chunk);
+  
+});
